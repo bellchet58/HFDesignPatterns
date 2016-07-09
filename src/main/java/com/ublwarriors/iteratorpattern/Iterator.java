48b94f9088c6d93e0afd7281e1714233bb8ee1ac
bellchet58 <bellchet@hotmail.com>
2016-07-09 17:19:44 +0800

    Rework diner menu with iterator

---
+++ src/main/java/com/ublwarriors/iteratorpattern/Iterator.java
@@ -0,0 +1,6 @@
+package com.ublwarriors.iteratorpattern;
+
+public interface Iterator {
+	public boolean hasNext();
+	public Object next();
+}
---
+++ src/main/java/com/ublwarriors/iteratorpattern/PancakeHouseMenuIterator.java
@@ -0,0 +1,33 @@
+package com.ublwarriors.iteratorpattern;
+
+import java.util.ArrayList;
+
+public class PancakeHouseMenuIterator implements Iterator{
+	
+	ArrayList<MenuItem> items;
+	
+	int position = 0;
+	
+	public PancakeHouseMenuIterator(ArrayList<MenuItem> items) {
+		this.items = items;
+	}
+	
+	@Override
+	public boolean hasNext() {
+		if(position >= items.size())
+		{
+			return false;
+		}
+		else {
+			return true;
+		}
+	}
+
+	@Override
+	public MenuItem next() {
+		MenuItem item = items.get(position);
+		position += 1;
+		return item;
+	}
+	
+}
